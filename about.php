f196920be0455722e7dff868d0951be817abf5d9
Sufei Zhang <sufeizhang92@gmail.com>
2016-09-22 13:31:56 -0500

    footer added to all pages

--- about.php
+++ about.php
@@ -1,64 +1,88 @@
 <?php /* Template Name: About */ get_header();?>
 <header id="masthead" class="site-header" role="banner">
-	  <div class="navbar-fixed">
-			<nav>
-				<div class="nav-wrapper">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="brand-logo center">LEEPT</a>
-					<a href="#" data-activates="mobile-demo" class="button-collapse">=</a>
-					<ul class="left hide-on-med-and-down">
-						<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
-						<li><a href="about">About</a></li>
-						<li><a href="faq">FAQ</a></li>
-						<li><a href="#">Blog</a></li>
-					</ul>
-					<ul class="right hide-on-med-and-down">
-						<li><a href="#contact">Contact</a></li>
-						<li><span>(123) 456 - 7890</span></li>
-					</ul>
-					<ul class="side-nav" id="mobile-demo">
-						<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
-						<li><a href="about">About</a></li>
-						<li><a href="faq">FAQ</a></li>
-						<li><a href="#">Blog</a></li>
-						<hr />
-						<li><a href="#contact">Contact</a></li>
-						<li><span>(123) 456 - 7890</span></li>
-					</ul>
-				</div>
-			</nav>
+	<div class="navbar-fixed">
+		<nav>
+			<div class="nav-wrapper">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="brand-logo center">LEEPT</a>
+				<a href="#" data-activates="mobile-demo" class="button-collapse">=</a>
+				<ul class="left hide-on-med-and-down">
+					<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
+					<li><a href="about">About</a></li>
+					<li><a href="faq">FAQ</a></li>
+					<li><a href="#">Blog</a></li>
+				</ul>
+				<ul class="right hide-on-med-and-down">
+					<li><a href="#contact">Contact</a></li>
+					<li><span>(123) 456 - 7890</span></li>
+				</ul>
+				<ul class="side-nav" id="mobile-demo">
+					<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
+					<li><a href="about">About</a></li>
+					<li><a href="faq">FAQ</a></li>
+					<li><a href="#">Blog</a></li>
+					<hr />
+					<li><a href="#contact">Contact</a></li>
+					<li><span>(123) 456 - 7890</span></li>
+				</ul>
+			</div>
+		</nav>
 		</div
 	</header><!-- #masthead -->
 
-<div class="about-header">
-	<span class="header-intro">ABOUT</span><br>
-</div>
-<div class="about-div">
-	<h3>Personal Background</h3>
-<p>James Lee was born and raised in the northern suburbs of Chicago. He graduated from the University of Illinois in Chicago with a bachelor’s degree in Movement Science and then received his Doctor of Physical Therapy degree from Rosalind Franklin University of Medicine and Science in 2010. He has enjoyed running since college and gave instructional seminars on running for his final project in PT school.</p>
-<p>James has also been an avid climber for over 10 years.  He has climbed in Squamish, B.C., Horseshoe Canyon, AR, Devil’s Lake, WI, Neceda, WI, DWS at Lake Kincaid, IL and regularly goes to Red River Gorge, KY.  He has had his fair share of tendon/ligament/muscle injuries so he knows exactly what climbers are going through when they get hurt.</p>
-</div>
+	<div class="about-header">
+		<span class="header-intro">ABOUT</span><br>
+	</div>
+	<div class="about-div">
+		<h3>Personal Background</h3>
+		<p>James Lee was born and raised in the northern suburbs of Chicago. He graduated from the University of Illinois in Chicago with a bachelor’s degree in Movement Science and then received his Doctor of Physical Therapy degree from Rosalind Franklin University of Medicine and Science in 2010. He has enjoyed running since college and gave instructional seminars on running for his final project in PT school.</p>
+		<p>James has also been an avid climber for over 10 years.  He has climbed in Squamish, B.C., Horseshoe Canyon, AR, Devil’s Lake, WI, Neceda, WI, DWS at Lake Kincaid, IL and regularly goes to Red River Gorge, KY.  He has had his fair share of tendon/ligament/muscle injuries so he knows exactly what climbers are going through when they get hurt.</p>
+	</div>
 
-<div class="about-picture">
-</div>
+	<div class="about-picture">
+	</div>
 
-<div class="about-div alt-div">
+	<div class="about-div alt-div">
 
-<h3>Fellowship Background</h3>
-<p>In November of  2012 James began his fellowship training with the Manual Therapy Institute (a fellowship program based in Texas credentialed by the American Physical Therapy Association) to specialize in manual therapy. This is more of a hands on approach to treating the body, restoring motion and improving movement.</p>
-<p>
-	James will be finishing his program by the end of 2016. Upon completion he will receive the title, Fellow of the America Association of Manual Physical Therapists. This is internationally recognized for the therapist’s competence and expertise in advanced clinical, analytical, and hands-on skills in the treatment of musculoskeletal orthopedic movement disorders.
-</p>
-</div>
+		<h3>Fellowship Background</h3>
+		<p>In November of  2012 James began his fellowship training with the Manual Therapy Institute (a fellowship program based in Texas credentialed by the American Physical Therapy Association) to specialize in manual therapy. This is more of a hands on approach to treating the body, restoring motion and improving movement.</p>
+		<p>
+			James will be finishing his program by the end of 2016. Upon completion he will receive the title, Fellow of the America Association of Manual Physical Therapists. This is internationally recognized for the therapist’s competence and expertise in advanced clinical, analytical, and hands-on skills in the treatment of musculoskeletal orthopedic movement disorders.
+		</p>
+	</div>
 
-<div class="about-div">
+	<div class="about-div">
 
-<h3>Clinical Background</h3>
-<p>James has been working in the outpatient orthopedic setting since 2010.  Within that time he has treated  a wide range of conditions including sports injuries, joint replacements, ligament sprain/reconstruction, neck/low back pain, muscle strains, fractures, etc.
- </p>
- <p>
- Due to his enjoyment for climbing, he has come to specialize in treating many climbing injuries from tendon strains, pulley injuries, shoulder pain, cervical nerve impingement, medial epicondylitis, etc. If there is one thing climbers hate the most is not being able to climb because of an injury. James can pinpoint where your injury is and teach you to still be able to safely climb while promote healing to your injured body parts.
- </p>
- <div class="center" style="margin-bottom:50px; margin-top: 100px;">
- <a class="button" href="faq">FAQ</a>
- </div>
-</div>
+		<h3>Clinical Background</h3>
+		<p>James has been working in the outpatient orthopedic setting since 2010.  Within that time he has treated  a wide range of conditions including sports injuries, joint replacements, ligament sprain/reconstruction, neck/low back pain, muscle strains, fractures, etc.
+		</p>
+		<p>
+			Due to his enjoyment for climbing, he has come to specialize in treating many climbing injuries from tendon strains, pulley injuries, shoulder pain, cervical nerve impingement, medial epicondylitis, etc. If there is one thing climbers hate the most is not being able to climb because of an injury. James can pinpoint where your injury is and teach you to still be able to safely climb while promote healing to your injured body parts.
+		</p>
+		<div class="center" style="margin-bottom:50px; margin-top: 100px;">
+			<a class="button" href="faq">FAQ</a>
+		</div>
+	</div>
+
+	<footer class="page-footer">
+    <div class="container center">
+      <div class="row footer-social">
+        <a class="social-icon" target="_blank" href="#"><img src="<?php bloginfo('template_url'); ?>/img/icon/facebook.png"/></a>
+        <a class="social-icon" target="_blank" href="#"><img src="<?php bloginfo('template_url'); ?>/img/icon/instagram.png"/></a>
+        <a class="social-icon" target="_blank" href="#"><img src="<?php bloginfo('template_url'); ?>/img/icon/google-plus.png"/></a>
+        <a class="social-icon" target="_blank" href="#"><img src="<?php bloginfo('template_url'); ?>/img/icon/wordpress.png"/></a>
+      </div>
+
+      <div class="row footer-nav">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a>
+        <a href="about">About</a>
+        <a href="faq">FAQ</a>
+        <a href="#">Blog</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </div>
+    <!-- <div class="footer-copyright">
+      <div class="container">
+        Copyright © 2016 LEEPT
+      </div>
+    </div> -->
+  </footer>
